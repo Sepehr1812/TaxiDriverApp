a873d71235d67460c0a1908b3e617f6a59be8357
sepehr <sepehrakhoundi1812@gmail.com>
2022-02-12 02:27:29 +0330

    add google map.

--- app/src/main/java/ir/snapp/snappboxtest/MainActivity.kt
+++ app/src/main/java/ir/snapp/snappboxtest/MainActivity.kt
@@ -1,11 +1,30 @@
 package ir.snapp.snappboxtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import ir.snapp.snappboxtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    // region of properties
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    // END of region of properties
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
     }
 }
